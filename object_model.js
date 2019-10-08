df807eac72ee1867996b8178534eb911eeb03bf8
47137352 <b-m.i.smith@accenturefederal.com>
2019-10-08 15:41:46 -0600

    rename object model

---
+++ object_model.js
@@ -0,0 +1,15 @@
+class Playlist {
+    constructor(id, name, ownerName, length) {
+      this.id = id;
+      this.name = name;
+      this.ownerName = ownerName;
+      this.length = length;
+    }
+  }
+  
+  class Track {
+    constructor(id, name) {
+      this.id = id;
+      this.name = name;
+    }
+  }
