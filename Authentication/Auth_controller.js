114fd0e7a2349742e765d685519e778391ff1bc5
47137352 <b-m.i.smith@accenturefederal.com>
2019-10-11 17:55:09 -0600

    new redirect

--- Authentication/Auth_controller.js
+++ Authentication/Auth_controller.js
@@ -6,7 +6,7 @@ app.controller('appController', function($scope, $http) {
         var authURL = 'https://accounts.spotify.com/authorize'
         authURL = authURL + '?client_id=7092039feff042d786be755e2282734f';
         //authURL = authURL + '&redirect_uri=file%3A%2F%2F%2FC%3A%2FUsers%2Fb-m.i.smith%2FDesktop%2FGitHub%2FGit%2520Repo%2FPlayListMaker%2FPlayListMaker%2FAuthentication%2FAuth.html';
-        authURL = authURL + '&redirect_uri=https%3A%2F%2Fwww.google.com%2F';
+        authURL = authURL + '&redirect_uri=https%3A%2F%2Finternetcreations.github.io%2FPlayListMaker%2FPlaylistMixer%2FPlaylistMixer.html';
         authURL = authURL + '&response_type=token'; 
         
         window.location.href = authURL;
