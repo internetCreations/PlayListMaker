680ae4a93b9c8235b03663b79150570929d92789
47137352 <b-m.i.smith@accenturefederal.com>
2019-10-11 19:12:51 -0600

    authurl

--- Authentication/Auth_controller.js
+++ Authentication/Auth_controller.js
@@ -5,11 +5,10 @@ app.controller('appController', function($scope, $http) {
         //alert('test');
         var authURL = 'https://accounts.spotify.com/authorize'
         authURL = authURL + '?client_id=7092039feff042d786be755e2282734f';
-        //authURL = authURL + '&redirect_uri=file%3A%2F%2F%2FC%3A%2FUsers%2Fb-m.i.smith%2FDesktop%2FGitHub%2FGit%2520Repo%2FPlayListMaker%2FPlayListMaker%2FAuthentication%2FAuth.html';
         authURL = authURL + '&redirect_uri=https%3A%2F%2Finternetcreations.github.io%2FPlayListMaker%2FPlaylistMixer%2FPlaylistMixer.html';
         authURL = authURL + '&response_type=token'; 
         authURL = authURL + '&scope=user-modify-playback-state'; 
-        
+        alert(authURL); 
         window.location.href = authURL;
       }
 });
